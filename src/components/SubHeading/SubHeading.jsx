d7378f07da6a99f3ffeaac854cfe428a769279e1
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-09-20 01:30:21 +0200

    Fixing typos

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import {images} from '../../constants'
+import { images } from '../../constants';
 
-const SubHeading = ({title}) => (
-  <div style={{marginBottom: '1rem'}}>
-    <p className='p__cormorant'> {title} </p>
-    <img src={images.spoon} alt='cuillere' className='spoon__img'/>
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="cuillere_image" className="spoon__img" />
   </div>
 );
 
