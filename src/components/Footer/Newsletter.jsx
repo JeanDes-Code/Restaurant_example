c6c32d92331b9be6a30c0e18b0feaac8d97e1491
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-09-20 15:13:52 +0200

    Footer ok

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title='Newsletter'/>
+      <h1 className='headtext__cormorant'>Abonnez-vous à notre Newsletter</h1>
+      <p className='p__opensans'>Pour ne jamais louper les dernières mises à jours !</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type='email' placeholder='Entrez votre adresse mail'/>
+      <button type='button' className='custom__button' >S'abonner</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi'
 
+import {FooterOverlay, Newsletter} from '../../components'
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+    
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+        <h1 className='app__footer-headtext'> Nous Contacter </h1>
+        <p className='p__opensans'>43 bis Rue Gambetta, 64200 Biarritz</p>
+        <p className='p__opensans'>+33 650-446-668</p>
+        <p className='p__opensans'>+33 650-446-668</p>
+      </div>
+      <div className='app__footer-links_logo'>
+        <img src={images.gericht} alt='footer_logo' />
+        <p className='p__opensans'>
+          « La meilleure façon de se trouver est de se perdre au service d'autrui. »
+        </p>
+        <img src={images.spoon} alt='spoon' className='spoon__img' style={{marginTop: 15}}/>
+        <div className='app__footer-links_icons'>
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className='app__footer-links_work'>
+        <h1 className='app__footer-headtext'> Horraires d'Ouverture </h1>
+        <p className='p__opensans'>Lundi-Vendredi: </p>
+        <p className='p__opensans'>10:00 am - 01:00 am</p>
+        <p className='p__opensans'>Samedi-Dimanche:</p>
+        <p className='p__opensans'>10:00 am - 02:00 am</p>
+      </div>
+    </div>
+
+    <div className='footer__copyright'>
+      <p className='p__opensans'>2022 Gericht. Tous Droits Réservés.</p>
+    </div>
   </div>
 );
 
